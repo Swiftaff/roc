209895f04cb8b509f25f3d485ba99f674e759fbf
Swiftaff <swiftaff@gmail.com>
2022-09-27 22:51:39 +0800

    try changing to_closure_data

--- crates/compiler/can/src/expr.rs
+++ crates/compiler/can/src/expr.rs
@@ -433,20 +433,42 @@ impl UpdaterData {
         // into
         //
         // (\r, v -> { r & foo: v })
+
+        //attempting to make a HashMap I guess?
+        let mut updates = SendMap::default();
+        let region = Region::new(
+            roc_region::all::Position::zero(),
+            roc_region::all::Position::zero(),
+        );
+        let field_value = Field {
+            var: field_var,
+            region,
+            loc_expr: Box::new(Loc::at_zero(Expr::Var(name))),
+        };
+
+        updates.insert(field.clone(), field_value);
+
+        let body = Expr::Update {
             record_var,
             ext_var,
-            field_var,
-            loc_expr: Box::new(Loc::at_zero(Expr::Var(record_symbol))),
-            field,
+            symbol: record_symbol,
+            updates,
         };
 
         let loc_body = Loc::at_zero(body);
 
-        let arguments = vec![(
-            record_var,
-            AnnotatedMark::known_exhaustive(),
-            Loc::at_zero(Pattern::Identifier(record_symbol)),
-        )];
+        let arguments = vec![
+            (
+                record_var,
+                AnnotatedMark::known_exhaustive(),
+                Loc::at_zero(Pattern::Identifier(record_symbol)),
+            ),
+            (
+                ext_var,
+                AnnotatedMark::known_exhaustive(),
+                Loc::at_zero(Pattern::Identifier(record_symbol)),
+            ),
+        ];
 
         ClosureData {
             function_type: function_var,
